d9e997e43fa33ac39c24b7dc73b0f14394681ae7
ruiiur <2362641334@qq.com>
2017-08-16 00:41:20 +0800

    js

--- main.js
+++ main.js
@@ -6,17 +6,24 @@ window.onload=function(){
     $('.esp').click(function(event){
         $('.category-list').slideToggle(100);
         $(this).children('i').toggleClass('change-bg');
-        // console.log($('.box').className);
         $('.box').toggleClass('show');
         $('body').css({'overflow':"hidden"});
+        event.stopPropagation();
+    })
+    //阻止冒泡事件
+    $('.esp ul li').click(function(event){
         event.stopPropagation();
     })
     //点击除了全部品类外其他页面部分使折叠菜单消失
     $('*:not(".esp")').click(function(){
             $('.category-list').slideUp(100);
             $('.esp').children('i').removeClass('change-bg');
-            $('.box').removeClass('show');
-            $('body').css({'overflow':"scroll"});
+    })
+
+    //除了点击页面上的哪些元素外去掉遮罩和使页面可以滚动
+    $('*:not(".esp,.search-btn")').click(function(){
+        $('.box').removeClass('show');
+        $('body').css({'overflow':"scroll"});
     })
 
     //导航切换
@@ -34,13 +41,9 @@ window.onload=function(){
         event.stopPropagation();
     });
     //取消搜索门店
-    $('*:not(".search*,.search-btn")').click(function(){
+    $('.box').click(function(){
         $('.search').slideUp(200);
-        $('.box').removeClass('show');
-        $('body').css({'overflow':"scroll"});
-    })
-
-    //取消搜索门店
+    });
     $('.cancel').click(function(event){
         //animate执行完一个动画再执行另一个函数
         // $('.search').animate({left:"100%"},300,function(){
@@ -52,10 +55,12 @@ window.onload=function(){
         event.stopPropagation();
     });
 
-    //显示或收缩活动
+    //距离最近页显示或收缩活动
     $.each($('.stores-list li'),function(index,params) {
+        //获取全部活动的ul高度
         var oldHeight = $('.stores-list li:eq(' + index + ')').find('.activity').children('ul').height();
-        console.log($('.activity ul li').get(0).offsetHeight);
+        // console.log($('.activity ul li').get(0).offsetHeight);
+        //如果全部活动的ul高度大于两个li则执行if里面的操作
         if ($('.stores-list li:eq(' + index + ')').find('.activity').children('ul').height() >= $('.activity ul li').get(0).offsetHeight * 2) {
             var flag=true;
             $('.stores-list li:eq(' + index + ')').find('.activity').children('ul').siblings(".all-activity").show();
